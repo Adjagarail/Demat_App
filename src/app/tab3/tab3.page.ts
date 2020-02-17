1fba9f0d18e229e02f15529bcef21b8a0244bf8e
Yaya  SADIO <ysadio@gainde2000.sn>
2020-02-17 10:13:52 +0000

    ajout sponsor et partenaires

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -2,31 +2,44 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-tab3',
-  templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss'],
+  selector: "app-tab3",
+  templateUrl: "tab3.page.html",
+  styleUrls: ["tab3.page.scss"]
 })
 export class Tab3Page {
   APropos: any;
-  public donneees:any = [];
+  public donneees: any = [];
+  public donneeees: any = [];
   link_postere = "http://app.joinpuzzle.com/public/uploads/images/postere/";
+  link_sponsor = "http://app.joinpuzzle.com/public/uploads/images/sponsor/";
   api_Propos = "http://app.joinpuzzle.com/public/api/apropos.json";
+  api_Sponsor = "http://app.joinpuzzle.com/public/api/sponsors.json";
+  sliderConfig = {
+    slidesPerView: 1.6,
+    spaceBetween: 5,
+    centeredSlides: true
+  };
+  private cartItem: any;
 
   constructor(public http: HttpClient) {
-    console.log('Ici la connexion à l\'api tous passe nickel');
-    this.getPropos()
+    console.log("Ici la connexion à l'api tous passe nickel");
+    this.getPropos();
+    this.getSponsor();
   }
 
   // Fetch tous les Données issues de l'entité A Propos
   getPropos() {
     return new Promise(resolve => {
-      this.http.get(this.api_Propos).subscribe(data => {
-        resolve(data);
-        this.donneees = data
-        console.log(this.donneees);
-      }, err => {
-        console.log(err);
-      });
+      this.http.get(this.api_Propos).subscribe(
+        data => {
+          resolve(data);
+          this.donneees = data;
+          console.log(this.donneees);
+        },
+        err => {
+          console.log(err);
+        }
+      );
     });
   }
   // Pour la Actualisation et la reactualisation des Données
@@ -39,7 +52,22 @@ export class Tab3Page {
         .catch(() => {
           event.target.complete();
         });
-    }, 3000)
+    }, 3000);
+  }
+  // Fetch tous les Données issues de l'entité Sponsor et Partenaires
 
+  getSponsor() {
+    return new Promise(resolve => {
+      this.http.get(this.api_Sponsor).subscribe(
+        data => {
+          resolve(data);
+          this.donneeees = data;
+          console.log(this.donneeees);
+        },
+        err => {
+          console.log(err);
+        }
+      );
+    });
   }
 }
